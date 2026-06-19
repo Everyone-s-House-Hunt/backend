261938d9a1979fa7728219f677a2585270e8572f
toma1128 <kirima--idea--@au.com>
2026-06-19 14:24:52 +0900

    [add]ユーザーのdtoの作成

---
+++ app/dto/user_dto.go
@@ -0,0 +1,20 @@
+package dto
+
+type RegisterRequest struct {
+	Username string `json:"username" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=6"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
+}
+
+type LoginResponse struct {
+	Token string `json:"token"`
+}
+
+type GoogleLoginRequest struct {
+	IDToken string `json:"id_token" binding:"required"`
+}
