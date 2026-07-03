7cecba084dcaaee7bb9020accffc5b02f9cecb08
iuhouh <kobaken20040105@icloud.com>
2026-07-03 16:30:10 +0900

    [fix] DB接続時のリトライ処理を追加し、MySQLの準備を待つように修正

--- app/utils/connect.go
+++ app/utils/connect.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"fmt"
 	"log"
+	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -18,7 +19,18 @@ func InitDB() *gorm.DB {
 	dbname := os.Getenv("DB_NAME")
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true&loc=Asia%%2FTokyo", user, password, host, port, dbname)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	// コンテナ起動直後は MySQL の準備が終わっていないことがあるため、リトライして待つ
+	var db *gorm.DB
+	var err error
+	for attempt := 1; attempt <= 10; attempt++ {
+		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err == nil {
+			break
+		}
+		log.Printf("DB接続待機中 (%d/10): %v", attempt, err)
+		time.Sleep(3 * time.Second)
+	}
 	if err != nil {
 		log.Fatalf("接続設定エラー: %v", err)
 	}
